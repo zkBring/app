b06a707da0b30f2d0608569b9a0bf003269bedda
spacehaz <spacehaz@gmail.com>
2025-02-28 16:38:51 +0300

    added updates for enc keys

--- src/app/drops/[drop_id]/content/index.tsx
+++ src/app/drops/[drop_id]/content/index.tsx
@@ -26,23 +26,35 @@ const Content: FC<TProps> = ({
   console.log({
     drop
   })
+  const {
+    title,
+    campaign_id,
+    expiration_time,
+    symbol,
+    token_amount,
+    token_standard,
+    creator_address,
+    token_address,
+    encrypted_multiscan_qr_enc_code,
+    encrypted_multiscan_qr_secret
+  } = drop
 
 // encrypted_multiscan_qr_enc_code: "GMqe7zrdsrNp"
 // encrypted_multiscan_qr_secret: "3rf9bbAuiHyF"
 
-  const link = '/verify/3rf9bbAuiHyF/GMqe7zrdsrNp'
+  const link = `/verify/${encrypted_multiscan_qr_secret}/${encrypted_multiscan_qr_enc_code}`
   return <ThemeProvider theme={dark}>
     <Header />
     <Container>
-    title {drop.title} <br />
-    campaign_id {drop.campaign_id} <br />
-    expiration_time {drop.expiration_time} <br />
-    symbol {drop.symbol} <br />
-    token_amount {drop.token_amount}<br />
-    token_standard {drop.token_standard}<br />
+    title {title} <br />
+    campaign_id {campaign_id} <br />
+    expiration_time {expiration_time} <br />
+    symbol {symbol} <br />
+    token_amount {token_amount}<br />
+    token_standard {token_standard}<br />
 
-    creator_address {drop.creator_address}<br />
-    token_address {drop.token_address}<br />
+    creator_address {creator_address}<br />
+    token_address {token_address}<br />
     
     Go to check verification <LinkStyled href={link}>here</LinkStyled>
     </Container>
